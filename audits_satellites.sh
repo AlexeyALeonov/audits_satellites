9f145750be145fbcce031d865f9e27620318a88d
Alexey A. Leonov <aaleonov@yandex.ru>
2019-08-08 10:50:07 +0300

    added a bash version of audits_satellites
    added a bash version of audits_satellites

---
+++ audits_satellites.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "Fetching satellite audits stat information. Please wait..."
+
+audits=$(mktemp)
+sats=$(mktemp)
+if [ "$1" ]; then
+  logs=$(mktemp)
+  docker logs storagenode 2>$logs
+else
+  logs=$1
+fi
+cat $logs | grep GET_AUDIT > $audits
+ 
+cat $audits | cut -f5 | cut -f5 -d " " | cut -f2 -d "\"" > ${sats}
+known_sats=$(sort ${sats} | uniq)
+ 
+for sat in $known_sats; do
+  echo -e "Sat ID:\t\t\t\t$sat"
+  echo -e "Unrecoverable Failed Audits:\t$(cat "$audits" | grep failed | grep open | grep -c "$sat")"
+  echo -e "Recoverable Failed Audits:\t$(cat "$audits" | grep failed | grep -v open | grep -c "$sat")"
+  echo -e "Successful Audits:\t\t$(cat "$audits" | grep downloaded | grep -c "$sat")"
+done
+ 
+rm $sats
+rm $audits
