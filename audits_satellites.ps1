f6d60a7c5395c53337f1da4cb4daa3138c25aa07
Alexey A. Leonov <aaleonov@yandex.ru>
2019-08-25 18:02:29 +0300

    replaced aliases to the actual commands

--- audits_satellites.ps1
+++ audits_satellites.ps1
@@ -8,12 +8,12 @@ if (-not $Path) {
     $logs = Get-Content $Path
 }
 
-$audits = ($logs | sls GET_AUDIT).Line
-$sats = (($audits | sls '({.*})').Matches.Value | ConvertFrom-Json).SatelliteID | select -Unique
+$audits = ($logs | Select-String GET_AUDIT).Line
+$sats = (($audits | Select-String '({.*})').Matches.Value | ConvertFrom-Json).SatelliteID | Select-Object -Unique
  
 foreach ($sat in $sats) {
    "Satellite:`t`t`t`t" + $sat
-   "Unrecoverable Failed Audits:`t" + ($audits | sls failed | sls open | sls $sat).Count
-   "Recoverable Failed Audits:`t" + ($audits | sls failed | sls open -NotMatch | sls $sat).Count
-   "Successful Audits:`t`t" + ($audits | sls downloaded | sls $sat).Count
+   "Unrecoverable Failed Audits:`t" + ($audits | Select-String failed | Select-String open | Select-String $sat).Count
+   "Recoverable Failed Audits:`t" + ($audits | Select-String failed | Select-String open -NotMatch | Select-String $sat).Count
+   "Successful Audits:`t`t" + ($audits | Select-String downloaded | Select-String $sat).Count
 }
